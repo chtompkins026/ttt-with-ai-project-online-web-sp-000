f63466471c75d7057f44fdae1875087440ffd6c5
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-01 21:37:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,36 +1,57 @@
 module Players
-
   class Computer < Player
     attr_reader :board
 
     WIN_COMBINATIONS = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
     ]
 
     def move(board)
+      @board = board
 
-      return '4' if board.cells.all?{|e| e == " "}
+      opponent_token
 
-      WIN_COMBINATIONS.each do |combo|
-        if combo.map {|idx| board.cells[idx]}.count(@token) >= 1 && combo.any? {|e| board.cells[e] == " "}
-          return combo.select {|idx| board.cells[idx] == " "}.first.to_s
+      if good_move(self.token) != nil
+        number = good_move(self.token)
+      elsif good_move(opponent_token) != nil
+        number = good_move(opponent_token)
+      else
+        number = free_move
+      end
+      number += 1
+      return number.to_s
+    end
+
+    def opponent_token
+      self.token == "X" ? "O" : "X"
+    end
+
+    def good_move(token)
+        winning_move = nil
+
+        WIN_COMBINATIONS.detect do |win_combo|
+           winning_move = win_combo.detect { |e| board.cells[e] == " " }
+          if winning_move && win_combo.count{ |e| board.cells[e] == token } >= 2
+            return winning_move
+          end
         end
+
+        winning_move
       end
 
-    #   #First Move - Hard Coding This Shizzzz
-    #   if board.cells[4] == " "
-    #     return "4"
-    #   else
-    #     return "0"
-    #   end
-    # end #end of the move board
-  end
-end
-end
+    def free_move
+      board.cells[4] == " " ? 4 : board.cells.index {|index| index == " "}
+    end
+
+
+  end #end of module
+
+end #end of Class
+
